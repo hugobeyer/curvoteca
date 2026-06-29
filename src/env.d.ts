d99f13f47a9fc86bca9cb246849ad2ecf826ebbb
hugobeyer <hugobeyer@gmail.com>
2026-06-29 08:02:25 -0300

    detail popup

--- src/env.d.ts
+++ src/env.d.ts
@@ -11,6 +11,16 @@ declare global {
     KEY_GRID_SIZE: string;
     KEY_GRID_COLS: string;
     CurvotecaRenderCurveView: (view: Element) => void;
+    __detailOverlay: {
+      buildSnippetTabs: (
+        container: HTMLElement,
+        entry: { id: string; name: string; equation: string; previewPoints: [number, number][]; snippets?: Record<string, string>; [k: string]: unknown },
+        onSelect: (key: string, code: string) => void,
+      ) => void;
+      showCode: (codeEl: HTMLElement, copyEl: HTMLElement, code: string) => void;
+      copyDetailCode: (text: string) => Promise<void>;
+      flashCopied: (el: HTMLElement) => void;
+    };
   }
 }
 
